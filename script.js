9c6c1732f68d6db1c1c800194b98fd83a8b7fc93
KWOMsm <73637543+KWOMsm@users.noreply.github.com>
2024-11-21 14:35:42 +0900

    카톡 버튼 제거, 문제 및 영상 추가

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ let score = 0;
 const questions = [
    {
         question: '조현병을 가진 사람은 범죄를 일으킬 확률이 높다.',
-        feedback_yes: '아쉽습니다. 이것은 잘못된 인식입니다. 실제로 조현병을 가진 사람들은 범죄의 가해자보다 피해자가 되는 경우가 더 많습니다.',
-        feedback_no: '맞습니다! 실제로 조현병을 가진 사람들은 범죄의 가해자보다 피해자가 되는 경우가 더 많습니다.',
+        feedback_yes: '아쉽습니다. 이것은 잘못된 인식입니다. 2021년 기준 정신장애 범죄자는 전체 범죄자의 0.7%에 불과하였습니다.',
+        feedback_no: '맞습니다! 2021년 기준 정신장애 범죄자는 전체 범죄자의 0.7%에 불과하였습니다.',
         video: 'https://www.youtube.com/embed/6eB1-0oiwxY'
     },
     {
-        question: '조현병 환자는 치료를 받으면 완치될 수 있다.',
-        feedback_yes: '아쉽습니다. 조현병은 완치보다는 관리가 가능한 정신 질환입니다.',
-        feedback_no: '맞습니다! 조현병은 완치보다는 관리가 가능한 정신 질환입니다. 대부분의 환자들은 치료를 통해 증상을 조절할 수 있습니다.',
+        question: '조현병은 치료되지 않는 병이다.',
+        feedback_yes: '아쉽습니다. 조현병은 치료를 통해 증상을 조절할 수 있는 정신 질환입니다. 치료를 위해서는 약물 치료와 사회재활 치료를 함께 받는 것이 큰 도움이 됩니다.',
+        feedback_no: '맞습니다! 조현병은 치료를 통해 증상을 조절할 수 있는 정신 질환입니다. 치료를 위해서는 약물 치료와 사회재활 치료를 함께 받는 것이 큰 도움이 됩니다.',
         video: 'https://www.youtube.com/embed/aXnIoZVGcp4'
     },
     {
-        question: '조현병은 정신적으로 약한 사람에게만 발생한다.',
-        feedback_yes: '아쉽습니다. 이는 잘못된 인식입니다. 조현병은 여러 요인이 복합적으로 작용하여 발생합니다.',
-        feedback_no: '맞습니다! 조현병은 유전적, 환경적 요인이 복합적으로 작용하여 발생하는 질환입니다. 누구에게나 발생할 수 있습니다.',
+        question: '조현병은 정신적으로 마음이 약한 사람에게만 발생한다.',
+        feedback_yes: '아쉽습니다. 이는 잘못된 인식입니다. 조현병은 뇌의 기능에 문제가 생겨 유발되는 질환입니다. 마음을 강하게 먹는다고 해서 완치되지 않습니다.',
+        feedback_no: '맞습니다! 조현병은 뇌의 기능에 문제가 생겨 유발되는 질환입니다. 누구에게나 발생할 수 있는 질병입니다.',
         video: 'https://www.youtube.com/embed/aXnIoZVGcp4'
     },
     {
@@ -27,16 +27,16 @@ const questions = [
     },
     // 새로운 문제들 추가
     {
-        question: '조현병은 \'분열된 인격\'을 의미한다.',
-        feedback_yes: '아쉽습니다. 이는 잘못된 인식입니다. 조현병은 다중 인격장애가 아니라 현실 인식의 장애를 특징으로 하는 정신 질환입니다.',
-        feedback_no: '맞습니다! 조현병은 다중 인격장애가 아닙니다. 이는 현실 인식의 장애를 특징으로 하는 정신 질환입니다.',
-        video: '관련 유튜브 영상 URL'
+        question: '조현병은 드문 병이다.',
+        feedback_yes: '아쉽습니다. 이는 잘못된 인식입니다. 조현병은 100명 중에 1명 꼴로 누구라도 걸릴 수 있는 비교적 흔한 병입니다.',
+        feedback_no: '맞습니다! 조현병은 100명 중에 1명 꼴로 누구라도 걸릴 수 있는 비교적 흔한 병입니다.',
+        video: 'https://www.youtube.com/embed/Vyf48SbUJTg'
     },
     {
-        question: '조현병 환자는 항상 환각이나 망상 증상을 보인다.',
-        feedback_yes: '아쉽습니다. 모든 조현병 환자가 항상 환각이나 망상을 경험하는 것은 아닙니다. 증상은 개인마다, 시기마다 다를 수 있습니다.',
-        feedback_no: '맞습니다! 증상의 종류와 정도는 개인마다, 시기마다 다르며, 많은 환자들이 적절한 치료를 통해 증상을 조절하며 살아갑니다.',
-        video: '관련 유튜브 영상 URL'
+        question: '조현병 환자는 일상생활이 불가능하다.',
+        feedback_yes: '아쉽습니다. 조현병 환자들은 치료를 시작하고 치료계획을 충실히 따르면 일상생활, 사회관계 등의 활동이 모두 가능합니다.',
+        feedback_no: '맞습니다! 조현병 환자들은 치료를 시작하고 치료계획을 충실히 따르면 일상생활, 사회관계 등의 활동이 모두 가능합니다.',
+        video: 'https://www.youtube.com/embed/0eukcaOK5ls'
     }
 ];
 
@@ -117,9 +117,6 @@ function showResults() {
                     </button>
                     <button onclick="shareToX('${shareText}')" class="share-button x">
                         <i class="fab fa-x-twitter"></i> X에 공유하기
-                    </button>
-                    <button onclick="shareToKakao('${shareText}')" class="share-button kakao">
-                        <i class="fas fa-comment"></i> 카카오톡 공유하기
                     </button>
                 </div>
             </div>
